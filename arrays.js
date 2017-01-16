7cc198dabb093036507dc08f54172662b6fac04f
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-16 16:52:26 -0500

    Done.

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles' ]
+
+function addElementToBeginningOfArray (arr, ele) {
+  return [ele, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+  arr.unshift(ele)
+  return arr
+}
+
+function addElementToEndOfArray (arr, ele) {
+  return [...arr, ele,]
+}
+
+function destructivelyAddElementToEndOfArray(arr, ele) {
+  arr.push(ele)
+  return arr
+}
+
+function accessElementInArray (arr, i) {
+  return arr[i]
+}
+
+function removeElementFromEndOfArray (arr) {
+  return arr.slice(0, arr.length - 1)
+}
+
+function removeElementFromBeginningOfArray (arr) {
+  return arr.slice(1)
+}
